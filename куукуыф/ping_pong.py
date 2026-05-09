c9541eb2148bc8e532f61c5263df219880ca1392
cvaaas <n7793429@gmail.com>
2026-05-09 20:41:52 +0300

    Add files via upload

---
+++ куукуыф/ping_pong.py
@@ -0,0 +1,29 @@
+from pygame import *
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+FPS = 60
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height)
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (width, heigth))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
+
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+        display.update()
+        clock.tick(FPS)
